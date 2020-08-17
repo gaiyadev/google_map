7540abf0a7d53cb5b570151ed24d3ad778096248
gaiyadev <gaiyaobed94@gmail.com>
2020-08-17 18:31:25 +0100

    Littel fix

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:google_map/location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+Location location = Location();
 
 void main() => runApp(MyApp());
 
@@ -24,21 +26,23 @@ class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(location.latitude, location.longitude),
     zoom: 14.4746,
   );
 
   static final CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
+      target: LatLng(location.latitude, location.longitude),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
+    print(location.latitude);
+
     return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.satellite,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
